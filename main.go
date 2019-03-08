91bc892f8c4e8299c74d12b97c4481dc4057aaab
Dmitriy Zaharov <goddeuce1@gmail.com>
2019-03-09 01:46:46 +0300

    Changed routers

--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"github.com/valyala/fasthttp"
-
-	"./router"
+        "net/http"
+        "./api"
+        "github.com/gorilla/mux"
+        //"github.com/gorilla/handlers"
+        "github.com/rs/cors"
 )
 
 func main() {
-	port := "8080"
-	fmt.Println("Api running on port", port)
-	log.Fatal(fasthttp.ListenAndServe(":"+port, router.Instance.Handler))
+        c := cors.New(cors.Options {
+                AllowedOrigins: []string{"http://localhost:8000/"},
+                AllowedMethods: []string{"GET, POST, OPTIONS"},
+                AllowedHeaders: []string{"Content-Type", "Accept", "Origin"},
+                AllowCredentials: true,
+                OptionsPassthrough: true,
+        })
+
+
+        router := mux.NewRouter()
+        router.HandleFunc("/api/user/check", api.CheckSession).Methods("GET")
+        router.HandleFunc("/api/user/signup", api.CreateSession).Methods("OPTIONS, POST")
+
+
+	fmt.Println("Api running on port 8080")
+	http.ListenAndServe(":8080", c.Handler(router))
 }
