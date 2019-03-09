3d90122b7bfef216c465ea51b3628af6a6017aa7
Vekshin Roman <vekshin.roman@student.bmstu.ru>
2019-03-09 15:05:10 +0300

    Роутинг Score API

--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
-        "net/http"
-        "./api"
-        "github.com/gorilla/mux"
-        "github.com/gorilla/handlers"
+	"net/http"
+
+	"./api"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-        router := mux.NewRouter()
-        router.HandleFunc("/api/user/check", api.CheckSession).Methods("GET")
-        router.HandleFunc("/api/user/signup", api.CreateSession).Methods("POST", "OPTIONS")
-        router.HandleFunc("/api/user/login", api.GetSession).Methods("POST", "OPTIONS")
-        router.HandleFunc("/api/user/logout", api.DestroySession).Methods("GET")
+	router := mux.NewRouter()
+	router.HandleFunc("/api/user/check", api.CheckSession).Methods("GET")
+	router.HandleFunc("/api/user/signup", api.CreateSession).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/user/login", api.GetSession).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/user/logout", api.DestroySession).Methods("GET")
 
-        headers := handlers.AllowedHeaders([]string{"Content-Type"})
-        origins := handlers.AllowedOrigins([]string{"http://localhost:8000"})
-        methods := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "OPTIONS", "PUT"})
+	router.HandleFunc("/api/score", api.GetNextAfter).Methods("GET")
+	router.HandleFunc("/api/score", api.CreateScore).Methods("POST", "OPTIONS")
+
+	headers := handlers.AllowedHeaders([]string{"Content-Type"})
+	origins := handlers.AllowedOrigins([]string{"http://localhost:8000"})
+	methods := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "OPTIONS", "PUT"})
 
 	fmt.Println("Api running on port 8080")
 	http.ListenAndServe(":8080", handlers.CORS(origins, headers, methods, handlers.AllowCredentials())(router))
