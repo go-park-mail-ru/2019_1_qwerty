5bf286d0dfe695034628c29e8ed94fe01979fc33
Vekshin Roman <vekshin.roman@student.bmstu.ru>
2019-04-08 20:36:53 +0300

    Разбор ошибок

---
+++ helpers/errors.go
@@ -0,0 +1,26 @@
+package helpers
+
+import (
+	"2019_1_qwerty/models"
+	"encoding/json"
+	"net/http"
+)
+
+// ErroRouter - Разбор ошибок
+func ErroRouter(w *http.ResponseWriter, resp *models.User, err *error, statusCode int) {
+	switch *err {
+	case nil:
+		json.NewEncoder(w).Encode(*resp)
+		w.WriteHeader(statusCode)
+	case models.EUserNE,
+		models.ENE:
+		json.NewEncoder(w).Encode(*err)
+		w.WriteHeader(http.StatusNotFound)
+	case models.EUserAE:
+		json.NewEncoder(w).Encode(*resp)
+		w.WriteHeader(http.StatusConflict)
+	default:
+		json.NewEncoder(w).Encode((*err).Error())
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
