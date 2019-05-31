8fba31deb012bf18205fe734a00c179a4e977e3b
Roman Vekshin <vekshin.roman@student.bmstu.ru>
2019-05-31 23:48:42 +0300

    Update user.go

--- main/handlers/user.go
+++ main/handlers/user.go
@@ -51,7 +51,7 @@ func init() {
 
 //UpdateAvatar - upload avatar to static folder
 func UpdateAvatar(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseMultipartForm(9223372036854775806)
+	err := r.ParseMultipartForm(5 * 1024 * 1025)
 	if err != nil {
 		log.Println("Ошибка при парсинге тела запроса", err)
 		ErrorMux(&w, r, http.StatusInternalServerError)
